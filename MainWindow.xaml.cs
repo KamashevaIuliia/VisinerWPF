2eb91514304fe8485ff1d8550327f2a1fb26624b
KamashevaIuliia <juliakam2012@gmail.com>
2020-08-07 21:49:07 +0300

    upload project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace KursovayaKamashevaVisiner
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click_Read(object sender, RoutedEventArgs e)
+        {
+            var result = VigenereCipher.VCode(WorkWithFile.ReadFile(), "скорпион", false);
+            TextDeshifr.Text = result;
+        }
+        private void Button_Click_Shifr(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TextForShifr.Text) && !String.IsNullOrEmpty(Key.Text))
+            {
+                var result = VigenereCipher.VCode(TextForShifr.Text, Key.Text);
+                TextShifr.Visibility = Visibility.Visible;
+                OnSave.Visibility = Visibility.Visible;
+                TextShifr.Text = result;
+               
+            }
+            else
+            {
+                MessageBox.Show("Даже поля заполнить не можешь?");
+            }
+
+
+        }
+        private void Button_Click_Save1(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TextDeshifr.Text))
+            {
+                WorkWithFile.SaveFile(TextDeshifr.Text);             
+                TextDeshifr.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Зачем тебе сохранять пустой файл?");
+            }
+            
+        }
+        private void Button_Click_Save2(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TextShifr.Text))
+            {
+                WorkWithFile.SaveFile(TextShifr.Text);
+                TextForShifr.Clear();
+                TextShifr.Clear();
+                Key.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Зачем тебе сохранять пустой файл?");
+            }
+            
+        }
+    }
+}
---
+++ WorkWithFile.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KursovayaKamashevaVisiner
+{
+    public class WorkWithFile
+    {
+        public static string ReadFile()
+        {
+            var fileContent = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                var fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream, Encoding.Default, true))
+                {
+                    fileContent = reader.ReadToEnd();
+                }
+            }
+            return fileContent;
+        } 
+        public static void SaveFile(string result)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = "c:\\";
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, result, Encoding.UTF8);
+            }
+            MessageBox.Show("Файл успешно сохранён!\nПуть файла: " + saveFileDialog.FileName);
+
+        } 
+    }
+}
